24eff03444efc61749788e6c66bb47b43707d081
Patrick Lehmann <lehmann@squibble.me>
2016-06-19 15:18:23 +0200

    kleine Fixes

--- app/helpers/squibble_admin/markup/general_helper.rb
+++ app/helpers/squibble_admin/markup/general_helper.rb
@@ -38,7 +38,7 @@ module SquibbleAdmin::Markup::GeneralHelper
   # Diese Methode kümmert sich um die einheitliche Ausgabe der Timestamps
   # für die Detailseiten von Datensätzen.
   def show_timestamps(show, resourze = resource)
-    render partial: 'helpers/squibble_admin/markup/general_helpershow_timestamps',
+    render partial: 'helpers/squibble_admin/markup/general_helper/show_timestamps',
            locals: { resource: resource, show: show }
   end
 end
--- app/helpers/squibble_admin/markup/google_helper.rb
+++ app/helpers/squibble_admin/markup/google_helper.rb
@@ -1,6 +1,6 @@
-module SquibbleAdmin::Markup::GeneralHelper
+module SquibbleAdmin::Markup::GoogleHelper
   def google_map_for_address(address, options = {})
-    render partial: 'helpers/global/markup/google_helper/google_map_for_address',
+    render partial: 'helpers/squibble_admin/markup/google_helper/google_map_for_address',
            locals: { resource: address, options: options }
   end
 end
