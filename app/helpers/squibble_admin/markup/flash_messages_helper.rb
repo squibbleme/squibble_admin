98fb7f775467519b3c7f49bd1eb10ee50adb254b
Patrick Lehmann <lehmann@squibble.me>
2016-06-23 10:41:03 +0200

    FIX: Typo

--- app/helpers/squibble_admin/markup/flash_messages_helper.rb
+++ app/helpers/squibble_admin/markup/flash_messages_helper.rb
@@ -1,7 +1,7 @@
 module SquibbleAdmin::Markup::FlashMessagesHelper
   def flash_messages( options = {} )
     return unless notice.present? || alert.present?
-    render partial: 'helpers/squibble_admin/markup/flassh_messages_helper/flash_messages',
+    render partial: 'helpers/squibble_admin/markup/flash_messages_helper/flash_messages',
            locals: { notice: notice, alert: alert, container: options[:container] }
   end
 end
--- lib/squibble_admin/version.rb
+++ lib/squibble_admin/version.rb
@@ -1,3 +1,3 @@
 module SquibbleAdmin
-  VERSION = '0.0.8'
+  VERSION = '0.0.9'
 end
