41c16e3c9ef6fbd19833996b7a15c246607beb03
Patrick Lehmann <lehmann@squibble.me>
2016-07-12 11:16:09 +0200

    Hinzufügen des admin :confirmation_note

---
+++ app/helpers/squibble_admin/admin/markup/confirmation_helper.rb
@@ -0,0 +1,20 @@
+# Helper für die Darstellung von Bestätigungsseiten
+# im Admin Bereich.
+#
+module Admin::Markup::ConfirmationHelper
+
+  def confirmation_note(path, options = {})
+    options[:data] = {}
+
+    options[:data][:method] = :post if options[:data][:method].nil?
+    options[:translation_key] = params[:action].to_sym if options[:translation_key].nil?
+
+    options[:cancel_path] = resource_path if options[:cancel_path].nil?
+
+    render partial: 'helpers/admin/markup/confirmation_helper/confirmation_note',
+           locals: {
+             path: path,
+             options: options
+           }
+  end
+end
--- lib/squibble_admin/version.rb
+++ lib/squibble_admin/version.rb
@@ -1,3 +1,3 @@
 module SquibbleAdmin
-  VERSION = '0.0.21'
+  VERSION = '0.0.22'
 end
