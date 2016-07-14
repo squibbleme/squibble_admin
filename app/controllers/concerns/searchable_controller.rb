1103b772aa79fb3a308e9e05d647557c16064f73
Patrick Lehmann <lehmann@squibble.me>
2016-07-14 08:26:29 +0200

    Kleiner Fix

--- app/controllers/concerns/searchable_controller.rb
+++ app/controllers/concerns/searchable_controller.rb
@@ -28,33 +28,3 @@ module SearchableController
     end
   end
 end
-module SearchableController
-  extend ActiveSupport::Concern
-  include SquibbleService
-
-  included do
-    has_scope :query do |controller, scope, value|
-      search_request = Search::Request::AdminUser.new(
-        query: value,
-        params: controller.params,
-        admin_user_id: controller.current_admin_user.id
-      )
-      search_request.save!
-
-      scope.simple_query( value )
-    end
-
-    rescue_from Elasticsearch::Transport::Transport::Errors::NotFound do |exception|
-      log(:error, "Error while doing a search query: #{exception.message}",
-        params: params,
-        exception: exception.message
-      )
-
-      render template: 'admin/errors/error_search',
-             status: 500,
-             locals: {
-               exception: exception
-             }
-    end
-  end
-end
