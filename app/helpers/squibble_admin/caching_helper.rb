b960e4aa3767f0737e261d826310664f698dada1
Patrick Lehmann <lehmann@squibble.me>
2016-08-22 11:58:35 +0200

    Erweiterung CacheHelper

--- app/helpers/squibble_admin/caching_helper.rb
+++ app/helpers/squibble_admin/caching_helper.rb
@@ -1,19 +1,21 @@
 module SquibbleAdmin::CachingHelper
-  def index_collection_cache_key_without_pagination( resource_clazz = resource_class, coll = collection )
+  def index_collection_cache_key_without_pagination( resource_clazz = resource_class, coll = collection, options = {} )
     [
       params,
       resource_clazz.to_s,
       coll.count,
-      coll.max(:updated_at).to_i
+      coll.max(:updated_at).to_i,
+      options
     ]
   end
 
-  def index_collection_cache_key_with_pagination( resource_clazz = resource_class, coll = collection)
+  def index_collection_cache_key_with_pagination( resource_clazz = resource_class, coll = collection, options = {})
     [
       params,
       resource_clazz.to_s,
       coll.total_count,
       coll.max(:updated_at).to_i,
+      options
     ]
   end
 end
