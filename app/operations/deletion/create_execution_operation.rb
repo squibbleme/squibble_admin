b595450b51a426d80afc5200bc1c36123f30fd59
Patrick Lehmann <lehmann@squibble.me>
2016-12-05 20:03:55 +0100

    Entfernen der DeletionEntry :debug Logs

--- app/operations/deletion/create_execution_operation.rb
+++ app/operations/deletion/create_execution_operation.rb
@@ -12,13 +12,11 @@ class Deletion::CreateExecutionOperation < ComposableOperations::Operation
         backend_application_id: backend_application_id
       )
 
-      if execution.save
-        log(:debug, "Successfully saved Delection::Exection for Backend::Application ##{backend_application_id}.", _default_log_attributes)
-      else
-        msg = "Unable to save Deletion::Execution: #{execution.errors.messages}"
-        log(:error, msg, _default_log_attributes(errors: execution.errors.messages))
-        halt msg
-      end
+      return if execution.save
+
+      msg = "Unable to save Deletion::Execution: #{execution.errors.messages}"
+      log(:error, msg, _default_log_attributes(errors: execution.errors.messages))
+      halt msg
     else
       msg = "There is already a Deletion::Execution for Backend::Application ##{backend_application_id} and this deletion_entry."
       log(:error, msg, _default_log_attributes)
--- lib/squibble_admin/version.rb
+++ lib/squibble_admin/version.rb
@@ -1,3 +1,3 @@
 module SquibbleAdmin
-  VERSION = '0.3.23'
+  VERSION = '0.3.24'
 end
