9284053c647a2163d31cd2e2c5fed62b8bb8ce04
Patrick Lehmann <lehmann@squibble.me>
2016-12-25 13:50:27 +0100

    Erweiterung Buttons um Aktionen zur Steuerung der Berechtigung

--- app/helpers/squibble_admin/markup/button_helper.rb
+++ app/helpers/squibble_admin/markup/button_helper.rb
@@ -1,27 +1,28 @@
 module SquibbleAdmin::Markup::ButtonHelper
 
-  def clone_button(resource, size = :default, path = new_resource_path(clone_by_id: resource.id))
+  def clone_button(resource, size = :default, path = new_resource_path(clone_by_id: resource.id), options = {})
+    action = options[:action].present? ? options[:action] = :create
     # return unless can? :create, resource
     render partial: 'helpers/squibble_admin/markup/button_helper/clone_button',
-           locals: {path: path, size: size, enabled: can?(:create, resource)}
+           locals: {path: path, size: size, enabled: can?(action, resource)}
   end
 
-  def destroy_button(resource, size = :default, path = resource_path(resource.id))
-    # return unless can? :destroy, resource
+  def destroy_button(resource, size = :default, path = resource_path(resource.id), options = {})
+    action = options[:action].present? ? options[:action] = :destroy
     render partial: 'helpers/squibble_admin/markup/button_helper/destroy_button',
-           locals: {path: path, size: size, enabled: can?(:destroy, resource)}
+           locals: {path: path, size: size, enabled: can?(action, resource)}
   end
 
-  def edit_button(resource, size = :default, path = edit_resource_path(resource.id))
-    # return unless can? :update, resource
+  def edit_button(resource, size = :default, path = edit_resource_path(resource.id), options = {})
+    action = options[:action].present? ? options[:action] = :update
     render partial: 'helpers/squibble_admin/markup/button_helper/edit_button',
-           locals: {path: path, size: size, enabled: can?(:update, resource)}
+           locals: {path: path, size: size, enabled: can?(action, resource)}
   end
 
-  def show_button(resource, size = :default, path = resource_path(resource.id))
-    # return unless can? :show, resource
+  def show_button(resource, size = :default, path = resource_path(resource.id), options = {})
+    action = options[:action].present? ? options[:action] = :show
     render partial: 'helpers/squibble_admin/markup/button_helper/show_button',
-           locals: {path: path, size: size, enabled: can?(:show, resource)}
+           locals: {path: path, size: size, enabled: can?(action, resource)}
   end
 
 end
